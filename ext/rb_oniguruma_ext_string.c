0bfcf2b714994fad930a198085fe9696fb9fe247
Geoff Garside <geoff.garside@openhosting.co.uk>
2007-09-19 15:03:37 +0000

    Attempt to resolve issues with String sub methods not being able to take blocks

--- ext/rb_oniguruma_ext_string.c
+++ ext/rb_oniguruma_ext_string.c
@@ -1,11 +1,56 @@
 #include <ruby.h>
 #include "rb_oniguruma_ext.h"
 
+typedef struct og_string_substitution_args {
+  VALUE oregexp;
+  VALUE method;
+  VALUE string;
+} og_StringSubstitutionArgs;
+
+#define og_StringSubstitutionArgs_set(args_, a, b, c) do {  \
+  og_StringSubstitutionArgs *ssap = (args_);                \
+  (ssap)->oregexp = a;                                      \
+  (ssap)->method = b;                                       \
+  (ssap)->string = c;                                       \
+} while(0)
+
+static VALUE
+og_oniguruma_string_do_substitution_block(og_StringSubstitutionArgs *args)
+{
+  VALUE argv[2];
+  
+  argv[0] = args->string;
+  argv[1] = (VALUE)NULL;
+  
+  return rb_funcall3(args->oregexp, args->method, 1, argv);
+}
+
+// Taken from examining some output from rb2cx
+static VALUE
+og_oniguruma_string_block_helper(VALUE bl_val, VALUE proc, VALUE self)
+{
+  if (ruby_current_node->nd_state != 1) {
+    if (bl_val == Qundef) bl_val = rb_ary_new2(0);
+    else {
+      VALUE tmp = rb_check_array_type(bl_val);
+      bl_val = (NIL_P(tmp) ? rb_ary_new3(1, bl_val) : tmp);
+    }
+  }
+  if (RARRAY(bl_val)->len == 0) return rb_funcall3(proc, rb_intern("call"), 0, 0);
+  else {
+    int argc = RARRAY(bl_val)->len;
+    VALUE *argv = ALLOCA_N(VALUE, argc);
+    MEMCPY(argv, RARRAY(bl_val)->ptr, VALUE, argc);
+    return rb_funcall3(proc, rb_intern("call"), argc, argv);
+  }
+}
+
 static VALUE
 og_oniguruma_string_do_substitution(VALUE self, char *method, int argc, VALUE *argv)
 {
   og_DefineConstantNames;
   VALUE oregexp, re, arg, oargv[2], nargv[3];
+  og_StringSubstitutionArgs fargs;
   
   og_ObtainConstants;
   if (rb_block_given_p()) {
@@ -16,17 +61,21 @@ og_oniguruma_string_do_substitution(VALUE self, char *method, int argc, VALUE *a
     rb_scan_args(argc, argv, "2", &re, &arg);
   }
   
-  /* Get the regular expression and make our initialization argv */
   oargv[0] = re;
   oargv[1] = (VALUE)NULL;
-  
-  nargv[0] = self;
-  nargv[1] = arg;
-  nargv[2] = (VALUE)NULL;
-  
-  // TODO: Find how to pass a block back to ruby
   oregexp = rb_class_new_instance(1, oargv, og_cOniguruma_ORegexp);
-  return rb_funcall3(oregexp, rb_intern(method), 2, nargv);
+  
+  if (rb_block_given_p()) {
+    og_StringSubstitutionArgs_set(&fargs, oregexp, rb_intern(method), self);
+    return rb_iterate(og_oniguruma_string_do_substitution_block, (VALUE)&fargs,
+      og_oniguruma_string_block_helper, arg);
+  } else {
+    nargv[0] = self;
+    nargv[1] = arg;
+    nargv[2] = (VALUE)NULL;
+
+    return rb_funcall3(oregexp, rb_intern(method), 2, nargv);
+  }
 }
 
 static VALUE
