9daf790c75b918c5df46f890e9ba48e104a7c640
Geoff Garside <geoff.garside@openhosting.co.uk>
2008-04-15 22:35:38 +0000

    Update major version

--- ext/rb_oniguruma_version.h
+++ ext/rb_oniguruma_version.h
@@ -1,7 +1,7 @@
 #ifndef _RB_ONIGURUMA_VERSION_H_
 #define _RB_ONIGURUMA_VERSION_H_
 
-#define OG_VERSION_MAJOR  0
+#define OG_VERSION_MAJOR  2
 #define OG_VERSION_MINOR  0
 #define OG_VERSION_TEENY  1
 
